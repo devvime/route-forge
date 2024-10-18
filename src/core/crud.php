d17117297a3f2745d1625d9a8f1bffbcf0cb1366
devvime <dihapp@outlook.com>
2024-10-18 00:11:19 -0300

    adjust CRUD class to use PDO and prepared statements

--- src/core/crud.php
+++ src/core/crud.php
@@ -1,74 +1,151 @@
 <?php
 
+// class CRUD
+// {
+
+//   private $conn;
+
+//   public function __construct(
+//     $db_host,
+//     $db_username,
+//     $db_password,
+//     $db_name
+//   ) {
+//     $this->conn = new mysqli(
+//       $db_host,
+//       $db_username,
+//       $db_password,
+//       $db_name
+//     );
+//   }
+
+//   public function create($table, $data)
+//   {
+//     $query = "INSERT INTO `$table` SET";
+//     foreach ($data as $field => $value) {
+//       $query .= "`$field` = '$value', ";
+//     }
+//     $query = rtrim($query, ', ');
+//     if ($this->conn->query($query)) {
+//       return true;
+//     } else {
+//       return false;
+//     }
+//   }
+
+//   public function read($table, $where = '')
+//   {
+//     $query = "SELECT * FROM `$table`";
+//     if (!empty($where)) {
+//       $query .= " WHERE $where";
+//     }
+//     $result = $this->conn->query($query);
+//     if ($result) {
+//       return $result->fetch_all();
+//     } else {
+//       return false;
+//     }
+//   }
+
+//   public function update($table, $data, $where)
+//   {
+//     $query = "UPDATE `$table` SET";
+//     foreach ($data as $field => $value) {
+//       $query .= "`$field` = '$value', ";
+//     }
+//     $query = rtrim($query, ', ');
+//     $query .= " WHERE $where";
+//     if ($this->conn->query($query)) {
+//       return true;
+//     } else {
+//       return false;
+//     }
+//   }
+
+//   public function delete($table, $where)
+//   {
+//     $query = "DELETE FROM `$table` WHERE $where";
+//     if ($this->conn->query($query)) {
+//       return true;
+//     } else {
+//       return false;
+//     }
+//   }
+// }
+
 class CRUD
 {
-
   private $conn;
 
-  public function __construct(
-    $db_host,
-    $db_username,
-    $db_password,
-    $db_name
-  ) {
-    $this->conn = new mysqli(
-      $db_host,
-      $db_username,
-      $db_password,
-      $db_name
-    );
+  public function __construct($db_host, $db_username, $db_password, $db_name)
+  {
+    try {
+      $dsn = "mysql:host=$db_host;dbname=$db_name;charset=utf8mb4";
+      $this->conn = new PDO($dsn, $db_username, $db_password);
+      $this->conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    } catch (PDOException $e) {
+      die("Connection failed: " . $e->getMessage());
+    }
   }
 
   public function create($table, $data)
   {
-    $query = "INSERT INTO `$table` SET";
-    foreach ($data as $field => $value) {
-      $query .= "`$field` = '$value', ";
-    }
-    $query = rtrim($query, ', ');
-    if ($this->conn->query($query)) {
+    $fields = implode(', ', array_keys($data));
+    $placeholders = implode(', ', array_fill(0, count($data), '?'));
+    
+    $query = "INSERT INTO `$table` ($fields) VALUES ($placeholders)";
+    
+    try {
+      $stmt = $this->conn->prepare($query);
+      $stmt->execute(array_values($data));
       return true;
-    } else {
+    } catch (PDOException $e) {
       return false;
     }
   }
 
-  public function read($table, $where = '')
+  public function read($table, $where = '', $params = [])
   {
     $query = "SELECT * FROM `$table`";
     if (!empty($where)) {
       $query .= " WHERE $where";
     }
-    $result = $this->conn->query($query);
-    if ($result) {
-      return $result->fetch_all();
-    } else {
+
+    try {
+      $stmt = $this->conn->prepare($query);
+      $stmt->execute($params);
+      return $stmt->fetchAll(PDO::FETCH_ASSOC);
+    } catch (PDOException $e) {
       return false;
     }
   }
 
-  public function update($table, $data, $where)
+  public function update($table, $data, $where, $whereParams = [])
   {
-    $query = "UPDATE `$table` SET";
-    foreach ($data as $field => $value) {
-      $query .= "`$field` = '$value', ";
-    }
-    $query = rtrim($query, ', ');
-    $query .= " WHERE $where";
-    if ($this->conn->query($query)) {
+    $setClause = implode(', ', array_map(fn($field) => "`$field` = ?", array_keys($data)));
+
+    $query = "UPDATE `$table` SET $setClause WHERE $where";
+
+    try {
+      $stmt = $this->conn->prepare($query);
+      $stmt->execute(array_merge(array_values($data), $whereParams));
       return true;
-    } else {
+    } catch (PDOException $e) {
       return false;
     }
   }
 
-  public function delete($table, $where)
+  public function delete($table, $where, $params = [])
   {
     $query = "DELETE FROM `$table` WHERE $where";
-    if ($this->conn->query($query)) {
+    
+    try {
+      $stmt = $this->conn->prepare($query);
+      $stmt->execute($params);
       return true;
-    } else {
+    } catch (PDOException $e) {
       return false;
     }
   }
 }
+
